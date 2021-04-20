192e32dc79565d079ebe98a03c2387713d33cc0a
Johan Carlsson <johan.carlsson89@gmail.com>
2021-04-20 13:44:48 +0100

    minor on target fixes.

--- startup.py
+++ startup.py
@@ -3,7 +3,7 @@ import os
 
 # boot up and application and reboot it if the screensaver kicks in.
 def start_process():
-    process = subprocess.Popen(os.environ['START_PROCESS'], shell=False)
+    process = subprocess.Popen(os.environ['START_PROCESS'].split(), shell=False)
     return process
 
 saver = subprocess.Popen(('xscreensaver-command', '-watch'), shell=False, stdout=subprocess.PIPE)
